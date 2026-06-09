32fd20c6100a5de439b15b362fc72d4ff1bf88a8
freaqbh <alfaruqiabhi111@gmail.com>
2026-06-09 15:22:02 +0700

    update endpoint

--- smaRT-frontend/src/services/api.ts
+++ smaRT-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://smart-api.alfaruqiabhi111.workers.dev/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
