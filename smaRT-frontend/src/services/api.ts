14ddecc6d539f6a2440bfc1a7a3e7f01de5d1601
freaqbh <alfaruqiabhi111@gmail.com>
2026-06-16 04:49:30 +0700

    change api endpoint

--- smaRT-frontend/src/services/api.ts
+++ smaRT-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://smart-api.alfaruqiabhi111.workers.dev/api',
+  baseURL: 'https://api.smart-rt.cloud/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
